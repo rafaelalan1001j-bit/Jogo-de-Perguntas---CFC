5e7becee2d46376944ccf62b50c7024eaac8a839
Rafa2.0 <rafaelalan1001j@gmail.com>
2026-01-20 22:56:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const botao = document.getElementById('btn-comecar');
+botao.addEventListener('click', function() {
+    console.log('O jogo vai começar!');
+    alert('Preparado para o desafio? O Quiz vai iniciar agora!');
+}
+);
+
