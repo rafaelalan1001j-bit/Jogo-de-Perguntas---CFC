bf73365d94991a1a0dd022c951f83e3fd2c28ca1
Rafa2.0 <rafaelalan1001j@gmail.com>
2026-01-22 18:45:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 const botao = document.getElementById('btn-comecar');
 
 botao.addEventListener('click', function() {
-    console.log('O jogo vai começar!');
-    alert('Preparado para o desafio? O Quiz vai iniciar agora!');
-    // Redireciona para a página do quiz
     window.location.href = "quiz.html";
 });
+
 
